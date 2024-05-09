3f901f5080142360cdd41bb236580883b8bdadbd
denballakh <denis.ballakh@gmail.com>
2024-05-09 22:27:45 +0300

    add more functions

--- src/math_parser.js
+++ src/math_parser.js
@@ -4,25 +4,57 @@ import lark from './_math_parser.js';
 let {get_parser} = lark;
 
 let transformer = {
-    number: ([n]) => function (x) { return parseFloat(n.value); },
-    var: ([s])  => function (x) { return x; }, // TODO: every variable is considered to be the same as x
+    number: function ([n]) {
+        const value = parseFloat(n.value);
+        return (x) => value;
+    },
+    var: function ([s]) {
+        switch (s.value) {
+            case "x": return (x) => x;
+            case "pi": return (x) => Math.PI;
+            case "e": return (x) => Math.E;
+            default: return (x) => NaN;
+        }
+    },
+    expr_unary:  function ([{value: op}, rhs]) {
+        switch (op) {
+            case "+": return (x) => +rhs(x);
+            case "-": return (x) => -rhs(x);
+            default: return (x) => NaN;
+        }
+    },
     expr_binary:  function ([lhs, {value: op}, rhs]) {
         console.log(lhs, op, rhs);
         switch (op) {
-            case "+": return function (x) { return lhs(x) + rhs(x); };
-            case "-": return function (x) { return lhs(x) - rhs(x); };
-            case "*": return function (x) { return lhs(x) * rhs(x); };
-            case "/": return function (x) { return lhs(x) / rhs(x); };
+            case "+": return (x) => lhs(x) + rhs(x);
+            case "-": return (x) => lhs(x) - rhs(x);
+            case "*": return (x) => lhs(x) * rhs(x);
+            case "/": return (x) => lhs(x) / rhs(x);
+            default: return (x) => NaN;
         }
     },
     expr_func_call:  function ([{value: name}, ...rhs]) {
         console.log(name, rhs);
         switch (name) {
-            case "min": return function (x) { return Math.min(rhs[0](x), rhs[1](x)); };
-            case "max": return function (x) { return Math.max(rhs[0](x), rhs[1](x)); };
-            case "log": return function (x) { return Math.log(rhs[0](x)); };
-            case "sin": return function (x) { return Math.sin(rhs[0](x)); };
-            case "cos": return function (x) { return Math.cos(rhs[0](x)); };
+            case "min": return (x) => Math.min(rhs[0](x), rhs[1](x));
+            case "max": return (x) => Math.max(rhs[0](x), rhs[1](x));
+
+            case "ln": return (x) => Math.log(rhs[0](x));
+            case "log10": return (x) => Math.log(rhs[0](x)) / Math.log(10);
+            case "log2": return (x) => Math.log(rhs[0](x)) / Math.log(2);
+            case "log": return (x) => Math.log(rhs[1](x)) / Math.log(rhs[0](x));
+
+            case "sin": return (x) => Math.sin(rhs[0](x));
+            case "cos": return (x) => Math.cos(rhs[0](x));
+            case "tan": return (x) => Math.tan(rhs[0](x));
+            case "cot": return (x) => 1 / Math.tan(rhs[0](x));
+
+            case "abs": return (x) => Math.abs(rhs[0](x));
+            case "sqrt": return (x) => Math.sqrt(rhs[0](x));
+
+            case "f": return (x) => x;
+            case "id": return (x) => x;
+            default: return (x) => NaN;
         }
     }
 };
