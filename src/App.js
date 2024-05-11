54132a94f2c5132dc8d9c1e6ca93c2227228275f
AnastasiayA26 <75423962+AnastasiayA26@users.noreply.github.com>
2024-05-11 19:18:02 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Plot from 'react-plotly.js';
 import MathKeyboard from './MathKeyboard';
-import {make_function} from './math_parser.js';
 import { evaluate } from 'mathjs';
-import './styles.css'
+import './styles.css';
 import './index.css';
-import ZoomControls, { handleZoomInX, handleZoomOutX, handleZoomInY, handleZoomOutY, handleResetZoom } from './components/ZoomControls';
+import {make_function} from './math_parser.js';
 
-function App() {
+const App = () => {
     const [functions, setFunctions] = useState([]);
     const [functionInput, setFunctionInput] = useState('');
     const [plotData, setPlotData] = useState([]);
     const [errorMessage, setErrorMessage] = useState('');
     const [xRange, setXRange] = useState([-100, 100]);
     const [yRange, setYRange] = useState([-100, 100]);
-    const [isKeyboardExpanded, setIsKeyboardExpanded] = useState(false);
     const [isFunctionListVisible, setIsFunctionListVisible] = useState(false);
     const [hiddenFunctions, setHiddenFunctions] = useState([]);
+    const [isKeyboardExpanded, setIsKeyboardExpanded] = useState(false);
+    const [keyboardButtonColor, setKeyboardButtonColor] = useState('#1a73e8');
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        calculatePlotData();
+    }, [functions, hiddenFunctions, xRange]);
 
     const calculatePlotData = () => {
         const traces = functions
@@ -29,10 +34,19 @@ function App() {
 
                 for (let x = xRange[0]; x <= xRange[1]; x += step) {
                     const replacedFunc = func.replace(/x/g, x).replace(/y/g, x);
-                    const y = eval(replacedFunc);
+                    const y = evaluate(replacedFunc);
                     xValues.push(x);
                     yValues.push(y);
                 }
+
+                let minY = yValues[0];
+                let maxY = yValues[0];
+                for (let y of yValues) {
+                    if (y < minY) minY = y;
+                    if (y > maxY) maxY = y;
+                }
+
+                setYRange([minY, maxY]);
 
                 return {
                     x: xValues,
@@ -51,110 +65,176 @@ function App() {
         setErrorMessage('');
     };
 
-    const addFunction = () => {
-        const newFunction = { func: functionInput, color: getRandomColor() };
-        setFunctions([...functions, newFunction]);
-        setFunctionInput('');
-        setIsFunctionListVisible(true);
-        calculatePlotData();
+    const handleFunctionEdit = (index, editedFunction) => {
+        setFunctions(prevFunctions => {
+            const updatedFunctions = [...prevFunctions];
+            updatedFunctions[index] = editedFunction;
+            return updatedFunctions;
+        });
     };
 
-    const editFunction = (index, editedFunction) => {
-        const updatedFunctions = [...functions];
-        updatedFunctions[index] = editedFunction;
-        setFunctions(updatedFunctions);
-        calculatePlotData();
+    const handleFunctionInputChange = (e) => {
+        setFunctionInput(e.target.value);
     };
 
-    const getRandomColor = () => {
-        const letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
+    const handleAddFunction = () => {
+        if (functionInput.trim() !== '') {
+            setFunctions([...functions, { func: functionInput, color: getRandomColor() }]);
+            setFunctionInput('');
+            setIsFunctionListVisible(true);
+        } else {
+            setErrorMessage('Введите функцию.');
         }
-        return color;
+    };
+
+    const FunctionList = ({ functions, hiddenFunctions }) => (
+        <div style={{ marginTop: '10px' }}>
+            <div style={{
+                backgroundColor: '#fff',
+                border: '1px solid #ddd',
+                borderRadius: '5px',
+                padding: '10px',
+                width: 'auto',
+                maxHeight: '200px',
+                overflowY: 'auto'
+            }}>
+                {functions.map((func, index) => (
+                    <div key={index} style={{ padding: '5px', display: 'flex', alignItems: 'center', cursor: 'pointer' }}>
+                        <div
+                            style={{
+                                width: '20px',
+                                height: '20px',
+                                borderRadius: '50%',
+                                backgroundColor: func.color,
+                                marginRight: '5px',
+                                cursor: 'pointer',
+                                border: '1px solid #ddd',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center'
+                            }}
+                            onClick={() => handleFunctionToggle(index)}
+                        >
+                            {hiddenFunctions.includes(index) && <span style={{ color: '#ddd', fontSize: '12px' }}>•</span>}
+                        </div>
+                        <span
+                            style={{
+                                fontStyle: hiddenFunctions.includes(index) ? 'italic' : 'normal',
+                                textDecoration: hiddenFunctions.includes(index) ? 'line-through' : 'none'
+                            }}
+                            contentEditable={!hiddenFunctions.includes(index)}
+                            suppressContentEditableWarning={true}
+                            onBlur={(e) => handleFunctionEdit(index, { ...func, func: e.target.textContent })}
+                        >
+                            {func.func}
+                        </span>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+
+    const handleFunctionToggle = (index) => {
+        setHiddenFunctions(prevHiddenFunctions => {
+            const updatedHiddenFunctions = [...prevHiddenFunctions];
+            const functionIndex = updatedHiddenFunctions.indexOf(index);
+            if (functionIndex === -1) {
+                updatedHiddenFunctions.push(index);
+            } else {
+                updatedHiddenFunctions.splice(functionIndex, 1);
+            }
+            return updatedHiddenFunctions;
+        });
+    };
+
+    const handleKeyboardButtonClick = () => {
+        setIsKeyboardExpanded(!isKeyboardExpanded);
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === "ArrowDown") {
+            setIsKeyboardExpanded(true);
+        }
+    };
+
+    const handleKeyUp = (event) => {
+        if (event.key === "ArrowDown") {
+            setIsKeyboardExpanded(false);
+        }
     };
 
     useEffect(() => {
-        calculatePlotData();
-    }, [functions, xRange, yRange, hiddenFunctions]);
-
-    const layout = {
-        width: '80%',
-        height: '80vh',
-        xaxis: {
-            title: '',
-            range: xRange,
-            zeroline: true,
-            zerolinecolor: '#000',
-            zerolinewidth: 2,
-            gridcolor: 'rgba(0, 0, 0, 0.1)',
-            gridwidth: 1,
-            tickangle: 0
-        },
-        yaxis: {
-            title: '',
-            range: yRange,
-            zeroline: true,
-            zerolinecolor: '#000',
-            zerolinewidth: 2,
-            gridcolor: 'rgba(0, 0, 0, 0.1)',
-            gridwidth: 1
-        },
-        autosize: true,
-        legend: {
-            x: 0,
-            y: 1,
-            xanchor: 'left',
-            yanchor: 'top'
+        if (isKeyboardExpanded && inputRef.current) {
+            inputRef.current.focus();
         }
-    };
+    }, [isKeyboardExpanded]);
 
     return (
-        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start', zIndex: '2'}}>
-            <div style={{marginBottom: '20px', marginLeft: '10px'}}>
-                <input
-                    type="text"
-                    placeholder="Введите функцию"
-                    value={functionInput}
-                    onChange={(e) => setFunctionInput(e.target.value)}
-                    style={{marginRight: '10px', padding: '10px', width: '100%'}}
-                />
-                <button
-                    onClick={addFunction}
-                    style={{
-                        padding: '10px 20px',
-                        fontSize: '16px',
-                        backgroundColor: '#1a73e8',
+        <div style={{ display: 'flex', height: '100vh' }} onKeyDown={handleKeyDown} onKeyUp={handleKeyUp} tabIndex={-1}>
+            <div style={{ flex: '1', height: '100%', borderRight: '1px solid #ccc' }}>
+                <div style={{display: 'flex', alignItems: 'center'}}>
+                    <input
+                        ref={inputRef}
+                        type="text"
+                        placeholder="Введите функцию"
+                        value={functionInput}
+                        onChange={handleFunctionInputChange}
+                        style={{marginRight: '10px', padding: '10px', width: '80%', margin: '0'}}
+                    />
+                    <button onClick={handleAddFunction}
+                            style={{width: '20%', padding: '10px', backgroundColor: '#1a73e8', color: '#fff', margin: '0'}}>+
+                    </button>
+                </div>
+                {isFunctionListVisible && <FunctionList functions={functions} hiddenFunctions={hiddenFunctions}/>}
+            </div>
+            <div style={{flex: '4', height: '100%', position: 'relative'}}>
+                <Plot
+                    data={plotData}
+                    layout={{
+                        autosize: true,
+                        xaxis: {
+                            autorange: true,
+                            zeroline: true,
+                            zerolinecolor: '#000',
+                        },
+                        yaxis: {
+                            autorange: false,
+                            zeroline: true
 
-                        color: '#fff',
-                        border: 'none',
-                        borderRadius: '8px',
-                        cursor: 'pointer'
+,
+                            zerolinecolor: '#000',
+                            range: yRange,
+                            scaleanchor: 'x',
+                        }
                     }}
-                >
-                    add
-                </button>
-                <button
-                    onClick={() => setIsKeyboardExpanded(!isKeyboardExpanded)}
-                    style={{
-                        padding: '10px',
-                        fontSize: '16px',
-                        backgroundColor: '#1a73e8',
-                        color: '#fff',
-                        border: 'none',
-                        borderRadius: '5px',
-                        cursor: 'pointer'
-                    }}
-                >
-                    <span role="img" aria-label="keyboard-icon">
-                        ⌨️
-                    </span>
-                </button>
+                    useResizeHandler={true}
+                    style={{width: '100%', height: '100%'}}
+                />
+                {!isKeyboardExpanded &&
+                    <button
+                        onClick={handleKeyboardButtonClick}
+                        style={{
+                            position: 'fixed',
+                            bottom: '10px',
+                            left: '10px',
+                            padding: '10px',
+                            fontSize: '16px',
+                            backgroundColor: keyboardButtonColor,
+                            color: '#fff',
+                            border: 'none',
+                            borderRadius: '5px',
+                            cursor: 'pointer'
+                        }}
+                    >
+                        <span role="img" aria-label="keyboard-icon">
+                            ⌨️
+                        </span>
+                    </button>
+                }
             </div>
             {isKeyboardExpanded &&
                 <div style={{position: 'fixed', bottom: '20px', left: '20px', zIndex: '1'}}>
-                    <MathKeyboard onKeyClick={(key) => setFunctionInput(functionInput + key)}/>
+                    <MathKeyboard inputRef={inputRef} onKeyClick={(key) => setFunctionInput(functionInput + key)}/>
                     <div style={{textAlign: 'center', paddingTop: '5px'}}>
                         <span onClick={() => setIsKeyboardExpanded(false)} style={{cursor: 'pointer'}}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
@@ -165,94 +245,18 @@ function App() {
                     </div>
                 </div>
             }
-            <div style={{
-                position: 'fixed',
-                left: '80%',
-                top: '50%',
-                transform: 'translate(-50%, -50%)',
-                width: '80%',
-                height: '100vh',
-                zIndex: '0'
-            }}>
-                <Plot
-                    data={plotData}
-                    layout={layout}
-                    onRelayout={(event) => {
-                        if (event['xaxis.range[0]'] && event['xaxis.range[1]']) {
-                            setXRange([event['xaxis.range[0]'], event['xaxis.range[1]']]);
-                        }
-                        if (event['yaxis.range[0]'] && event['yaxis.range[1]']) {
-                            setYRange([event['yaxis.range[0]'], event['yaxis.range[1]']]);
-                        }
-                    }}
-                />
-            </div>
-
-            <FunctionList
-                functions={functions}
-                hiddenFunctions={hiddenFunctions}
-                onFunctionToggle={(index) => {
-                    const updatedFunctions = [...hiddenFunctions];
-                    if (updatedFunctions.includes(index)) {
-                        updatedFunctions.splice(updatedFunctions.indexOf(index), 1);
-                    } else {
-                        updatedFunctions.push(index);
-                    }
-                    setHiddenFunctions(updatedFunctions);
-                }}
-                onFunctionEdit={editFunction}
-            />
-            {errorMessage && <p style={{color: 'red'}}>{errorMessage}</p>}
         </div>
     );
-}
+};
 
-const FunctionList = ({functions, hiddenFunctions, onFunctionToggle, onFunctionEdit}) => (
-    <div style={{marginTop: '20px'}}>
-        <div style={{
-            backgroundColor: '#fff',
-            border: '1px solid #ddd',
-            borderRadius: '5px',
-            padding: '10px',
-            width: 'auto',
-            maxHeight: '200px',
-            overflowY: 'auto'
-        }}>
-            {functions.map((func, index) => (
-                <div key={index} style={{padding: '5px', display: 'flex', alignItems: 'center', cursor: 'pointer'}}>
-                    <div
-                        style={{
-                            width: '20px',
-                            height: '20px',
-                            borderRadius: '50%',
-                            backgroundColor: func.color,
-                            marginRight: '5px',
-                            cursor: 'pointer',
-                            border: '1px solid #ddd',
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center'
-                        }}
-                        onClick={() => onFunctionToggle(index)}
-                    >
-                        {hiddenFunctions.includes(index) && <span style={{ color: '#ddd', fontSize: '12px' }}>•</span>}
-                    </div>
-                    <span
-                        style={{
-                            fontStyle: hiddenFunctions.includes(index) ? 'italic' : 'normal',
-                            textDecoration: hiddenFunctions.includes(index) ? 'line-through' : 'none'
-                        }}
-                        contentEditable={!hiddenFunctions.includes(index)}
-                        suppressContentEditableWarning={true}
-                        onBlur={(e) => onFunctionEdit(index, { ...func, func: e.target.textContent })}
-                    >
-                        {func.func}
-                    </span>
-                </div>
-            ))}
-        </div>
-    </div>
-);
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 export default App;
 
