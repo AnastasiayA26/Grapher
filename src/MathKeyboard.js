173d3c7fde0c3582b72c168783ba10f4f7186772
AnastasiayA26 <75423962+AnastasiayA26@users.noreply.github.com>
2024-05-11 19:19:33 +0300

    Update MathKeyboard.js

--- src/MathKeyboard.js
+++ src/MathKeyboard.js
@@ -1,17 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
+import './MathKeyboard.css';
 
-const MathKeyboard = ({ onKeyClick }) => {
+const MathKeyboard = ({ onKeyClick, inputRef }) => {
     const [expanded, setExpanded] = useState(true);
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
     const [buttonColor, setButtonColor] = useState(Array(20).fill('#ffffff')); // Assuming 20 buttons, adjust if needed
     const [clickedButtonIndex, setClickedButtonIndex] = useState(null);
+    function handleDelete() {
+        if (!inputRef) return;
+
+        const input = inputRef.current;
+        const selectionStart = input.selectionStart;
+
+        const currentValue = input.value;
+        const newValue = currentValue.substring(0, selectionStart - 1) + currentValue.substring(selectionStart);
+        const newSelectionStart = selectionStart;
+
+        input.value = newValue;
+        input.setSelectionRange(newSelectionStart, newSelectionStart);
+        onKeyClick('');
+    }
 
     const handleKeyClick = (key, index) => {
-        if (key === '\u232b') { // символ "⌫" (значок клавиатуры для удаления)
-            onKeyClick('delete');
+        if (key === '\u232b') {
+            onKeyClick('');
         } else {
             let expression = '';
-            if (key === 'delete') {
+            if (key === '') {
                 expression = key;
             } else {
                 if (['sqrt', 'sin', 'cos', 'tan', 'ctg', 'ln', 'log'].includes(key)) {
@@ -46,30 +61,24 @@ const MathKeyboard = ({ onKeyClick }) => {
         };
     }, []);
 
-    // Calculate the width of each button dynamically based on the window width
-    const buttonWidth = Math.floor((windowWidth / 5) * 0.8); // Divide by the number of buttons in a row
+    const buttonWidth = Math.floor((windowWidth / 5) * 0.7);
 
     const buttonStyle = {
         flex: '1',
         padding: '10px',
-        fontSize: '16px',
+        fontSize: windowWidth <= 480 ? '12px' : '16px',
         backgroundColor: '#ffffff',
         color: '#333333',
         border: '1px solid #cccccc',
         borderRadius: '5px',
         cursor: 'pointer',
         margin: '2px',
-        width: `${buttonWidth}px` // Set the width dynamically
+        width: `${buttonWidth}px`
     };
 
     return (
-        <div style={{ position: 'relative' }}>
-            <div style={{
-                display: expanded ? 'flex' : 'none',
-                flexDirection: 'row',
-                justifyContent: 'center',
-                height: '100%'
-            }}>
+        <div style={{display: 'flex', height: '50%'}}>
+            <div className={expanded ? 'keyboard-expanded' : 'keyboard-collapsed'}>
                 <div style={{
                     display: 'flex',
                     flexDirection: 'column',
@@ -77,55 +86,54 @@ const MathKeyboard = ({ onKeyClick }) => {
                     padding: '10px',
                     backgroundColor: '#f2f2f2',
                     borderRadius: '10px',
-                    flex: '1'
+                    flex: expanded ? '1' : '0'
                 }}>
                     <div style={{display: 'flex', flexWrap: 'wrap'}}>
                         <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 0 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('x', 0)}>x</button>
+                                onClick={() => handleKeyClick('x', 0)} onKeyDown={() => inputRef.current.focus()}>x</button>
                         <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 1 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('y', 1)}>y</button>
+                                onClick={() => handleKeyClick('y', 1)} onKeyDown={() => inputRef.current.focus()}>y</button>
                         <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 2 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('ln( )', 2)}>ln</button>
+                                onClick={() => handleKeyClick('ln( )', 2)} onKeyDown={() => inputRef.current.focus()}>ln</button>
                         <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 3 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('log(a, x)', 3)}>log</button>
+                                onClick={() => handleKeyClick('log(a, x)', 3)} onKeyDown={() => inputRef.current.focus()}>log</button>
                         <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 4 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('cos( )', 4)}>cos</button>
+                                onClick={() => handleKeyClick('cos( )', 4)} onKeyDown={() => inputRef.current.focus()}>cos</button>
                     </div>
                     <div style={{display: 'flex', flexWrap: 'wrap'}}>
                         <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 5 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('(', 5)}>(</button>
+                                onClick={() => handleKeyClick('(', 5)} onKeyDown={() => inputRef.current.focus()}>(</button>
                         <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 6 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick(')', 6)}>)</button>
+                                onClick={() => handleKeyClick(')', 6)} onKeyDown={() => inputRef.current.focus()}>)</button>
                         <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 7 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('<', 7)}>{"<"}</button>
+                                onClick={() => handleKeyClick('<', 7)} onKeyDown={() => inputRef.current.focus()}>{"<"}</button>
                         <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 8 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('>', 8)}>{">"}</button>
-                        <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 9 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('sin( )', 9)}>sin</button>
+                                onClick={() => handleKeyClick('>', 8)} onKeyDown={() => inputRef.current.focus()}>{">"}</button>
+                        <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 9 ? '#d3d1d1' : '#ffffff'}} onClick={() => handleKeyClick('sin( )', 9)} onKeyDown={() => inputRef.current.focus()}>sin</button>
                     </div>
                     <div style={{display: 'flex', flexWrap: 'wrap'}}>
                         <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 10 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('abs()', 10)}>|x|</button>
+                                onClick={() => handleKeyClick('abs()', 10)} onKeyDown={() => inputRef.current.focus()}>|x|</button>
                         <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 11 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('<=', 11)}>≤</button>
+                                onClick={() => handleKeyClick('<=', 11)} onKeyDown={() => inputRef.current.focus()}>≤</button>
                         <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 12 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('>=', 12)}>≥</button>
+                                onClick={() => handleKeyClick('>=', 12)} onKeyDown={() => inputRef.current.focus()}>≥</button>
                         <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 13 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('ctg()', 13)}>ctg</button>
+                                onClick={() => handleKeyClick('ctg()', 13)} onKeyDown={() => inputRef.current.focus()}>ctg</button>
                         <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 14 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('tan( )', 14)}>tan</button>
+                                onClick={() => handleKeyClick('tan( )', 14)} onKeyDown={() => inputRef.current.focus()}>tan</button>
                     </div>
                     <div style={{display: 'flex', flexWrap: 'wrap'}}>
                         <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 15 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('sqrt()', 15)}>√</button>
+                                onClick={() => handleKeyClick('sqrt()', 15)} onKeyDown={() => inputRef.current.focus()}>√</button>
                         <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 16 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('^', 16)}>^</button>
+                                onClick={() => handleKeyClick('^', 16)} onKeyDown={() => inputRef.current.focus()}>^</button>
                         <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 17 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('e', 17)}>e</button>
+                                onClick={() => handleKeyClick('e', 17)} onKeyDown={() => inputRef.current.focus()}>e</button>
                         <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 18 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('pi', 18)}>π</button>
+                                onClick={() => handleKeyClick('pi', 18)} onKeyDown={() => inputRef.current.focus()}>π</button>
                         <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 19 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('.', 19)}>,</button>
+                                onClick={() => handleKeyClick('.', 19)} onKeyDown={() => inputRef.current.focus()}>,</button>
                     </div>
                 </div>
 
@@ -139,46 +147,77 @@ const MathKeyboard = ({ onKeyClick }) => {
                     flex: expanded ? '1' : '0'
                 }}>
                     <div style={{display: 'flex', flexWrap: 'wrap'}}>
-                        <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 20 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('7', 20)}>7</button>
-                        <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 21 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('8', 21)}>8</button>
-                        <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 22 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('9', 22)}>9</button>
-                        <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 23 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('\u232b', 23)}>
-                            ⌫
+                        <button
+                            style={{...buttonStyle, backgroundColor: clickedButtonIndex === 20 ? '#d3d1d1' : '#ffffff'}}
+                            onClick={() => handleKeyClick('7', 20)} onKeyDown={() => inputRef.current.focus()}>7
+                        </button>
+                        <button
+                            style={{...buttonStyle, backgroundColor: clickedButtonIndex === 21 ? '#d3d1d1' : '#ffffff'}}
+                            onClick={() => handleKeyClick('8', 21)} onKeyDown={() => inputRef.current.focus()}>8
+                        </button>
+                        <button
+                            style={{...buttonStyle, backgroundColor: clickedButtonIndex === 22 ? '#d3d1d1' : '#ffffff'}}
+                            onClick={() => handleKeyClick('9', 22)} onKeyDown={() => inputRef.current.focus()}>9
+                        </button>
+                        <button
+                            style={{...buttonStyle, backgroundColor: clickedButtonIndex === 23 ? '#d3d1d1' : '#ffffff'}}
+                            onClick={handleDelete} onKeyDown={() => inputRef.current.focus()}>⌫
+                        </button>
+                    </div>
+
+                    <div style={{display: 'flex', flexWrap: 'wrap'}}>
+                        <button
+                            style={{...buttonStyle, backgroundColor: clickedButtonIndex === 24 ? '#d3d1d1' : '#ffffff'}}
+                            onClick={() => handleKeyClick('4', 24)} onKeyDown={() => inputRef.current.focus()}>4
+                        </button>
+                        <button
+                            style={{...buttonStyle, backgroundColor: clickedButtonIndex === 25 ? '#d3d1d1' : '#ffffff'}}
+                            onClick={() => handleKeyClick('5', 25)} onKeyDown={() => inputRef.current.focus()}>5
+                        </button>
+                        <button
+                            style={{...buttonStyle, backgroundColor: clickedButtonIndex === 26 ? '#d3d1d1' : '#ffffff'}}
+                            onClick={() => handleKeyClick('6', 26)} onKeyDown={() => inputRef.current.focus()}>6
+                        </button>
+                        <button
+                            style={{...buttonStyle, backgroundColor: clickedButtonIndex === 27 ? '#d3d1d1' : '#ffffff'}}
+                            onClick={() => handleKeyClick('*', 27)} onKeyDown={() => inputRef.current.focus()}>*
                         </button>
                     </div>
                     <div style={{display: 'flex', flexWrap: 'wrap'}}>
-                        <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 24 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('4', 24)}>4</button>
-                        <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 25 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('5', 25)}>5</button>
-                        <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 26 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('6', 26)}>6</button>
-                        <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 27 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('*', 27)}>*</button>
+                        <button
+                            style={{...buttonStyle, backgroundColor: clickedButtonIndex === 28 ? '#d3d1d1' : '#ffffff'}}
+                            onClick={() => handleKeyClick('1', 28)} onKeyDown={() => inputRef.current.focus()}>1
+                        </button>
+                        <button
+                            style={{...buttonStyle, backgroundColor: clickedButtonIndex === 29 ? '#d3d1d1' : '#ffffff'}}
+                            onClick={() => handleKeyClick('2', 29)} onKeyDown={() => inputRef.current.focus()}>2
+                        </button>
+                        <button
+                            style={{...buttonStyle, backgroundColor: clickedButtonIndex === 30 ? '#d3d1d1' : '#ffffff'}}
+                            onClick={() => handleKeyClick('3', 30)} onKeyDown={() => inputRef.current.focus()}>3
+                        </button>
+                        <button
+                            style={{...buttonStyle, backgroundColor: clickedButtonIndex === 31 ? '#d3d1d1' : '#ffffff'}}
+                            onClick={() => handleKeyClick('-', 31)} onKeyDown={() => inputRef.current.focus()}>-
+                        </button>
                     </div>
                     <div style={{display: 'flex', flexWrap: 'wrap'}}>
-                        <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 28 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('1', 28)}>1</button>
-                        <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 29 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('2', 29)}>2</button>
-                        <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 30 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('3', 30)}>3</button>
-                        <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 31 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('-', 31)}>-</button>
-                    </div>
-                    <div style={{display: 'flex', flexWrap: 'wrap'}}>
-                        <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 32 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('0', 32)}>0</button>
-                        <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 33 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('=', 33)}>=</button>
-                        <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 34 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('+', 34)}>+</button>
-                        <button style={{ ...buttonStyle, backgroundColor: clickedButtonIndex === 35 ? '#d3d1d1' : '#ffffff'}}
-                                onClick={() => handleKeyClick('/', 35)}>/</button>
+                        <button
+                            style={{...buttonStyle, backgroundColor: clickedButtonIndex === 32 ? '#d3d1d1' : '#ffffff'}}
+                            onClick={() => handleKeyClick('0', 32)} onKeyDown={() => inputRef.current.focus()}>0
+                        </button>
+                        <button
+                            style={{...buttonStyle, backgroundColor: clickedButtonIndex === 33 ? '#d3d1d1' : '#ffffff'}}
+                            onClick={() => handleKeyClick('=', 33)} onKeyDown={() => inputRef.current.focus()}>=
+                        </button>
+                        <button
+                            style={{...buttonStyle, backgroundColor: clickedButtonIndex === 34 ? '#d3d1d1' : '#ffffff'}}
+                            onClick={() => handleKeyClick('+', 34)} onKeyDown={() => inputRef.current.focus()}>+
+                        </button>
+                        <button
+                            style={{...buttonStyle, backgroundColor: clickedButtonIndex === 35 ? '#d3d1d1' : '#ffffff'}}
+                            onClick={() => handleKeyClick('/', 35)} onKeyDown={() => inputRef.current.focus()}>/
+                        </button>
                     </div>
                 </div>
             </div>
@@ -187,5 +226,4 @@ const MathKeyboard = ({ onKeyClick }) => {
 };
 
 export default MathKeyboard;
-
 
